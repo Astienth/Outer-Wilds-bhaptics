19c091a0244a92e7a9492c587cdd45bb6fd17e4b
Astienth <astienth@gmail.com>
2022-09-12 12:12:56 +0200

    initial config, modifying names

--- OWBhaptics/OWBhaptics.cs
+++ OWBhaptics/OWBhaptics.cs
@@ -1,21 +1,14 @@
 ﻿using OWML.Common;
 using OWML.ModHelper;
 
-namespace ModTemplate
+namespace OWBhaptics
 {
-    public class ModTemplate : ModBehaviour
+    public class OWBhaptics : ModBehaviour
     {
-        private void Awake()
-        {
-            // You won't be able to access OWML's mod helper in Awake.
-            // So you probably don't want to do anything here.
-            // Use Start() instead.
-        }
-
         private void Start()
         {
             // Starting here, you'll have access to OWML's mod helper.
-            ModHelper.Console.WriteLine($"My mod {nameof(ModTemplate)} is loaded!", MessageType.Success);
+            ModHelper.Console.WriteLine($"My mod {nameof(OWBhaptics)} is loaded!", MessageType.Success);
 
             // Example of accessing game code.
             LoadManager.OnCompleteSceneLoad += (scene, loadScene) =>
