656eb42df415562a881f0d565507bda0c2ed82c0
Astienth <astienth@gmail.com>
2022-09-12 12:51:12 +0200

    first test

---
+++ OWBhaptics/BhapticsTactsuit.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Reflection;
+using Bhaptics.Tact;
+using OWML.Common;
+using OWML.ModHelper;
+using UnityEngine;
+
+namespace OWBhaptics
+{
+
+    public class BhapticsTactsuit : ModBehaviour
+    {
+        public static bool suitDisabled = true;
+        public static bool systemInitialized = false;
+        // dictionary of all feedback patterns found in the bHaptics directory
+        public static Dictionary<string, FileInfo> FeedbackMap = new Dictionary<string, FileInfo>();
+#pragma warning disable CS0618 // remove warning that the C# library is deprecated
+        public static HapticPlayer hapticPlayer;
+#pragma warning restore CS0618 
+
+        public static RotationOption defaultRotationOption = new RotationOption(0.0f, 0.0f);
+
+        #region Initializers
+
+        public void Awake()
+        {
+            try
+            {
+#pragma warning disable CS0618 // remove warning that the C# library is deprecated
+                hapticPlayer = new HapticPlayer("OuterWilds_bhaptics", "OuterWilds_bhaptics");
+#pragma warning restore CS0618
+                suitDisabled = false;
+            }
+            catch
+            {
+                return;
+            }
+            RegisterAllTactFiles();
+            PlaybackHaptics("HeartBeat");
+        }
+
+        /**
+         * Registers all tact files in bHaptics folder
+         */
+        void RegisterAllTactFiles()
+        {
+            if (suitDisabled) { return; }
+            // Get location of the compiled assembly and search through "bHaptics" directory and contained patterns
+            string assemblyFile = Assembly.GetExecutingAssembly().Location;
+            string myPath = Path.GetDirectoryName(assemblyFile);
+            string configPath = myPath + "\\bHaptics";
+            DirectoryInfo d = new DirectoryInfo(configPath);
+            FileInfo[] Files = d.GetFiles("*.tact", SearchOption.AllDirectories);
+            for (int i = 0; i < Files.Length; i++)
+            {
+                string filename = Files[i].Name;
+                string fullName = Files[i].FullName;
+                string prefix = Path.GetFileNameWithoutExtension(filename);
+                if (filename == "." || filename == "..")
+                    continue;
+                string tactFileStr = File.ReadAllText(fullName);
+                try
+                {
+                    hapticPlayer.RegisterTactFileStr(prefix, tactFileStr);
+                }
+                catch (Exception e) { ModHelper.Console.WriteLine(e.ToString()); }
+
+                FeedbackMap.Add(prefix, Files[i]);
+            }
+            systemInitialized = true;
+        }
+        #endregion
+
+
+        #region PlayingHapticsEffects
+
+        public void PlaybackHaptics(string key, float intensity = 1.0f, float duration = 1.0f)
+        {
+            if (suitDisabled) { return; }
+            if (FeedbackMap.ContainsKey(key))
+            {
+                ScaleOption scaleOption = new ScaleOption(intensity, duration);
+                hapticPlayer.SubmitRegisteredVestRotation(key, key, defaultRotationOption, scaleOption);
+            }
+            else
+            {
+                ModHelper.Console.WriteLine("Feedback not registered: " + key);
+            }
+        }
+}
--- OWBhaptics/OWBhaptics.cs
+++ OWBhaptics/OWBhaptics.cs
@@ -10,14 +10,7 @@ namespace OWBhaptics
             // Starting here, you'll have access to OWML's mod helper.
             ModHelper.Console.WriteLine($"My mod {nameof(OWBhaptics)} is loaded!", MessageType.Success);
 
-            // Example of accessing game code.
-            LoadManager.OnCompleteSceneLoad += (scene, loadScene) =>
-            {
-                if (loadScene != OWScene.SolarSystem) return;
-                var playerBody = FindObjectOfType<PlayerBody>();
-                ModHelper.Console.WriteLine($"Found player body, and it's called {playerBody.name}!",
-                    MessageType.Success);
-            };
+            //testing bhaptics
         }
     }
 }
